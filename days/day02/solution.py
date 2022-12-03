4917c5387b7ccc3d609f5bd20d7023f589084890
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2022-12-03 07:49:05 +0100

    day 2

---
+++ days/day02/solution.py
@@ -0,0 +1,48 @@
+from collections import Counter
+
+
+def get_input() -> Counter[str]:
+    with open("input.txt", "r") as f:
+        return Counter([line.strip() for line in f.readlines()])
+
+
+def part1() -> int:
+    SHAPE_SCORES = {"X": 1, "Y": 2, "Z": 3}
+    WINNING_CASES = {"A Y", "B Z", "C X"}
+    DRAW_CASES = {"A X", "B Y", "C Z"}
+    input = get_input()
+
+    score = 0
+
+    for k, v in input.items():
+        score += SHAPE_SCORES[k[-1]] * v
+        if k in DRAW_CASES:
+            score += 3 * v
+        elif k in WINNING_CASES:
+            score += 6 * v
+
+    return score
+
+
+def part2() -> int:
+    SHAPE_SCORES = {"A": 1, "B": 2, "C": 3}
+    WINNING_CASES = {"A": "B", "B": "C", "C": "A"}
+    LOSING_CASES = {v: k for k, v in WINNING_CASES.items()}
+    input = get_input()
+
+    score = 0
+
+    for k, v in input.items():
+        if k[-1] == "X":
+            score += SHAPE_SCORES[LOSING_CASES[k[0]]] * v
+        elif k[-1] == "Y":
+            score += v * (SHAPE_SCORES[k[0]] + 3)
+        else:
+            score += v * (SHAPE_SCORES[WINNING_CASES[k[0]]] + 6)
+
+    return score
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
