5378c9cc8bb393e8364b960548a60948afe096f7
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2022-12-07 02:02:26 +0100

    day 6

---
+++ days/day06/solution.py
@@ -0,0 +1,32 @@
+def get_input() -> str:
+    with open("input.txt") as f:
+        return f.readline()
+
+
+def get_unique_str_index(data: str, size: int) -> int:
+    unique_str: list[str] = []
+
+    for i, char in enumerate(data):
+        if len(unique_str) == size:
+            return i
+
+        if char in unique_str:
+            unique_str = unique_str[unique_str.index(char) + 1 :]
+
+        unique_str.append(char)
+
+    return -1
+
+
+def part1(data: str) -> int:
+    return get_unique_str_index(data, 4)
+
+
+def part2(data: str) -> int:
+    return get_unique_str_index(data, 14)
+
+
+if __name__ == "__main__":
+    data = get_input()
+    print(f"Part 1: {part1(data)}")
+    print(f"Part 2: {part2(data)}")
