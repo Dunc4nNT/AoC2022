360384bcee754065399997848ca927a15b2adf6f
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2022-12-19 18:04:31 +0100

    refactor/speedup (~2ms)

--- days/day15/solution.py
+++ days/day15/solution.py
@@ -1,63 +1,62 @@
 import re
 from time import perf_counter
+from typing import NamedTuple
 
-from shapely import LineString, Point, Polygon, box, unary_union
+from shapely import LineString, MultiPolygon, Polygon, box, unary_union
 
 
-def get_data() -> dict[Point, Point]:
-    with open("input.txt") as f:
-        return {
-            Point(int(x1), int(y1)): Point(int(x2), int(y2))
+class Point(NamedTuple):
+    x: int
+    y: int
+
+
+def get_data() -> list[tuple[Point, Point]]:
+    with open("input.txt", "r") as f:
+        return [
+            (Point(int(x1), int(y1)), Point(int(x2), int(y2)))
             for x1, y1, x2, y2 in re.findall(
                 r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)",
                 f.read(),
             )
-        }
+        ]
 
 
-def get_manhattan_distance_to(point_0: Point, point_1: Point) -> int:
-    return abs(point_1.x - point_0.x) + abs(point_1.y - point_0.y)
+def get_sensor_polygon(sensor: Point, beacon: Point) -> Polygon:
+    d_mh = abs(beacon.x - sensor.x) + abs(beacon.y - sensor.y)
+
+    return Polygon(
+        [
+            (sensor.x, sensor.y - d_mh),  # up
+            (sensor.x + d_mh, sensor.y),  # right
+            (sensor.x, sensor.y + d_mh),  # down
+            (sensor.x - d_mh, sensor.y),  # left
+        ]
+    )
 
 
-def get_most_outer_points(sensor: Point, beacon: Point) -> list[tuple[int, int]]:
-    d_mh = get_manhattan_distance_to(sensor, beacon)
-
-    return [
-        (sensor.x, sensor.y - d_mh),  # up
-        (sensor.x + d_mh, sensor.y),  # right
-        (sensor.x, sensor.y + d_mh),  # down
-        (sensor.x - d_mh, sensor.y),  # left
-    ]
+def get_all_sensors_poly(data: list[tuple[Point, Point]]) -> Polygon | MultiPolygon:
+    return unary_union([get_sensor_polygon(sensor, beacon) for sensor, beacon in data])
 
 
-def part1(row: int, data: dict[Point, Point]) -> int:
-    all_sensors_list = []
-    beacons_on_row = set()
+def part1(row: int, data: list[tuple[Point, Point]]) -> int:
+    sensors_poly = get_all_sensors_poly(data)
+    beacons_on_row = {beacon for _, beacon in data if beacon.y == row}
 
-    for sensor, beacon in data.items():
-        all_sensors_list.append(Polygon(get_most_outer_points(sensor, beacon)))
-        if beacon.y == row:
-            beacons_on_row.add(beacon)
+    row_line = LineString(
+        [(sensors_poly.bounds[0], row), (sensors_poly.bounds[2], row)]
+    )
 
-    all_sensors_poly = unary_union(all_sensors_list)
-    min_x, min_y, max_x, max_y = all_sensors_poly.bounds
-    row_line = LineString([(min_x, row), (max_x, row)])
-
-    intersections = row_line.intersection(all_sensors_poly)
+    intersections = row_line.intersection(sensors_poly)
 
     return int(intersections.length) + 1 - len(beacons_on_row)
 
 
-def part2(lower_limit: int, upper_limit: int, data: dict[Point, Point]) -> int:
-    all_sensors_list = []
+def part2(lower_limit: int, upper_limit: int, data: list[tuple[Point, Point]]) -> int:
+    sensors_poly = get_all_sensors_poly(data)
 
-    for sensor, beacon in data.items():
-        all_sensors_list.append(Polygon(get_most_outer_points(sensor, beacon)))
-
-    all_sensors_poly = unary_union(all_sensors_list)
     square = box(lower_limit, lower_limit, upper_limit, upper_limit)
 
-    differences = square.difference(all_sensors_poly)
+    differences = square.difference(sensors_poly)
 
     if isinstance(differences, Polygon):
         central_point = differences.centroid
